f6275467bc0abf9bc8149c7bfe5eeb58213fbab3
jowens25 <jowens@novuspower.com>
2025-05-01 12:08:44 -0500

    updated serial read

--- comms/serRead.c
+++ comms/serRead.c
@@ -1,13 +1,5 @@
 
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <fcntl.h>
-// #include <unistd.h>
-// #include <termios.h>
-#include <string.h>
-// #include <stdint.h>
-// #include <stdbool.h>
 #include "axi.h"
 // reads n bytes where n is the size of the input array
 int serRead(int ser, char data[], size_t dataLength)
