a824e5d7a43e058632a7e3fb79cc8b20ff8c5e4e
jowens25 <jowens@novuspower.com>
2025-05-01 12:09:16 -0500

    updated ser write

--- comms/serWrite.c
+++ comms/serWrite.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <termios.h>
-#include <string.h>
-#include <stdint.h>
-#include <stdbool.h>
+
 #include "axi.h" // writes n bytes where n is the str len of the input
 int serWrite(int ser, char data[], size_t dataLength)
 {
