a2fd00ff025c0760a7131a859670fe26ffa3060b
jowens25 <jowens@novuspower.com>
2025-07-28 16:13:19 -0500

    variscite ports and ips

--- axi/readRegister.c
+++ axi/readRegister.c
@@ -10,7 +10,7 @@ int readRegister(int64_t addr, int64_t *data)
     char hexData[64] = {0};
     char hexChecksum[3] = {0};
 
-    int ser = serOpen("/dev/ttyUSB0");
+    int ser = serOpen("/dev/ttymxc3");
     if (ser == -1)
     {
 
