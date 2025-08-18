8e7912360de9c0248a78f9e6f216b1721f060beb
jowens25 <jowens@novuspower.com>
2025-08-18 12:27:13 -0500

    more

--- cli/cmd/raw.go
+++ cli/cmd/raw.go
@@ -23,7 +23,7 @@ to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("raw called")
 
-		api.SendRaw(args[0])
+		api.SendRaw(args[0], "SAVED")
 	},
 }
 
