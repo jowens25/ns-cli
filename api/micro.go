3905a28eb76f64e16f37da3ce936238079d89b3d
jowens25 <jowens@novuspower.com>
2025-08-18 15:59:38 -0500

    flush

--- api/micro.go
+++ api/micro.go
@@ -4,7 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"syscall"
+	"time"
 )
+
+func FlushSerialBuffer(f *os.File) error {
+	fd := int(f.Fd())
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, uintptr(fd),
+		uintptr(0x540B), uintptr(2)) // TCFLSH ioctl
+	if errno != 0 {
+		return errno
+	}
+	return nil
+}
 
 func MakeCommand(cmd string, param ...string) []byte {
 
@@ -34,14 +46,16 @@ func MicroWrite(command string, responseMarker string, parameter ...string) stri
 
 	f, err := os.OpenFile(mcu_port, os.O_RDWR, 0644)
 
+	FlushSerialBuffer(f)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
 	n, err := f.Write(cmd)
-	n, err = f.Write(cmd)
-	n, err = f.Write(cmd)
+
+	time.Sleep(time.Millisecond * 50)
 
 	if err != nil {
 		log.Fatal(err)
