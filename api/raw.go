e3aa8f084e749b2df08672a20131873e5abc6871
jowens25 <jowens@novuspower.com>
2025-08-18 09:50:24 -0500

    trying file io

--- api/raw.go
+++ api/raw.go
@@ -3,58 +3,24 @@ package api
 import (
 	"fmt"
 	"log"
-	"time"
-
-	"go.bug.st/serial"
+	"os"
 )
 
-var FileDescriptor string = "/dev/ttymxc2"
-
-func SendRaw(rawString string) {
-
-	write_data := make([]byte, 0, 64)
-	read_data := make([]byte, 64)
-
-	mode := &serial.Mode{
-		BaudRate: 115200,
-		DataBits: 8,
-		Parity:   serial.NoParity,
-		StopBits: serial.OneStopBit,
-	}
-
-	port, err := serial.Open(FileDescriptor, mode)
+func ReadLikeCat() {
+	file, err := os.OpenFile("/dev/ttymxc2", os.O_RDWR, 0)
 	if err != nil {
-		log.Fatal("serial open err: ", err)
+		log.Fatal(err)
 	}
-	defer port.Close()
+	defer file.Close()
 
-	port.SetReadTimeout(time.Millisecond * 100)
-
-	write_data = append(write_data, rawString...)
-	write_data = append(write_data, '\r')
-	write_data = append(write_data, '\n')
-
-	n, err := port.Write(write_data)
-
-	if err != nil {
-		log.Fatal("write error: ", err)
+	buffer := make([]byte, 256)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+		if n > 0 {
+			fmt.Print(string(buffer[:n]))
+		}
 	}
-
-	if n == 0 {
-		log.Fatal("response: none")
-	}
-
-	for read_data[len(read_data)] != '\n' {
-		n, err = port.Read(read_data)
-	}
-
-	if err != nil {
-		log.Fatal("read error: ", err)
-	}
-
-	if n == 0 {
-		log.Fatal("response: none")
-	}
-	read_string := string(read_data)
-	fmt.Println(read_string)
 }
