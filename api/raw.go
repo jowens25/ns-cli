787dc83b6c89f77471d7e83b072844d0e402d980
jowens25 <jowens@novuspower.com>
2025-08-18 12:25:52 -0500

    fixedmytype

--- api/raw.go
+++ api/raw.go
@@ -1,15 +1,18 @@
 package api
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
 )
 
-func SendRaw(rawString string) {
+func SendRaw(command string, responseMarker string, parameter ...string) {
 
 	//buffer := make([]byte, 128)
-	write_data := make([]byte, 0, 64)
+	write_data := make([]byte, 256)
+	read_data := make([]byte, 256)
 
 	file, err := os.OpenFile("/dev/ttymxc2", os.O_RDWR, 0)
 	if err != nil {
@@ -18,32 +21,39 @@ func SendRaw(rawString string) {
 	}
 	defer file.Close()
 
-	write_data = append(write_data, rawString...)
+	command = "$" + command
 
-	//checksum := CalculateChecksum(write_data)
-	//write_data = append(write_data, '*')
-	//write_data = append(write_data, checksum...)
+	if len(parameter) > 0 {
+		command = command + "=" + parameter[0]
+	}
+
+	write_data = append(write_data, command...)
+	checksum := CalculateChecksum(write_data)
+	write_data = append(write_data, '*')
+	write_data = append(write_data, checksum...)
 	write_data = append(write_data, '\r')
 	write_data = append(write_data, '\n')
 
-	n, err := file.Write(write_data)
-	fmt.Println(err)
-	fmt.Println("wrote: ", n)
+	_, err = file.Write(write_data)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = file.Read(read_data)
 
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
-	defer file.Close()
 
-	buffer := make([]byte, 0, 256)
-	for {
-		n, err := file.Read(buffer)
-		if err != nil {
-			break
-		}
-		if n > 0 {
-			fmt.Print(string(buffer[:n]))
-		}
+	fmt.Println(string(read_data))
+
+	scanner := bufio.NewScanner(bytes.NewReader(read_data))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		fmt.Println(line) // print first line??
+
 	}
 
 }
