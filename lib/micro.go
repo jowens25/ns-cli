e897045cb5d80e1f5ccb49cb442156f4350142ff
jowens25 <jowens@novuspower.com>
2025-09-11 12:29:53 -0500

    serial port conf

--- lib/micro.go
+++ lib/micro.go
@@ -4,11 +4,27 @@ import "C"
 import (
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	"go.bug.st/serial"
 )
 
+const MCU_ENV_VAR string = "MCU_SERIAL_PORT"
+
+var mcu_port string
+var exists bool
+
+func init() {
+
+	mcu_port, exists = os.LookupEnv(MCU_ENV_VAR)
+	if !exists {
+		os.Setenv(MCU_ENV_VAR, "/dev/ttymxc2")
+		fmt.Println("set default serial port: /dev/ttymxc2")
+	}
+}
+
+// command is the actual string so ex $BAUDNV
 func ReadWriteMicro(command string) string {
 
 	command = command + "\r\n"
@@ -19,8 +35,6 @@ func ReadWriteMicro(command string) string {
 		Parity:   serial.NoParity,
 		StopBits: serial.OneStopBit,
 	}
-
-	mcu_port := "/dev/ttymxc2"
 
 	read_data := make([]byte, 1024)
 
@@ -33,7 +47,7 @@ func ReadWriteMicro(command string) string {
 	port.ResetInputBuffer()
 	port.ResetOutputBuffer()
 
-	n, err := port.Write([]byte(command))
+	_, err = port.Write([]byte(command))
 
 	fmt.Println(command)
 
@@ -41,21 +55,11 @@ func ReadWriteMicro(command string) string {
 		log.Fatal(err)
 	}
 
-	if n > 0 {
-		//fmt.Println("wrote: ", n, " bytes")
-	}
-
-	n, err = port.Read(read_data)
+	_, err = port.Read(read_data)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	if n > 0 {
-		//fmt.Println("read: ", n, " bytes")
-	}
-
-	//fmt.Println(string(read_data))
 
 	lines := strings.Split(string(read_data), "\n")
 
