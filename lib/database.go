fb43e074008f51d9834b6c53bd6365a309f8c380
jowens25 <jowens@novuspower.com>
2025-09-16 13:53:40 -0500

    removing more user

--- lib/database.go
+++ lib/database.go
@@ -22,7 +22,7 @@ func initDataBase() {
 		log.Fatal("Failed to connect to database: ", err)
 	}
 
-	err = db.AutoMigrate(&User{}, &SnmpV1V2cUser{}, &SnmpV3User{}, &Ntp{}, &AllowedNode{})
+	err = db.AutoMigrate(&SnmpV1V2cUser{}, &SnmpV3User{}, &Ntp{}, &AllowedNode{})
 
 	if err != nil {
 		log.Fatal("Failed to migrate database: ", err)
@@ -42,71 +42,67 @@ func initDataBase() {
 
 func createDefaultUser() {
 
-	var userCount int64
-	db.Model(&User{}).Count(&userCount)
+	//var userCount int64
+	//db.Model(&User{}).Count(&userCount)
 
-	if userCount == 0 {
+	var user User
+	user.Username = AppConfig.User.DefaultUsername
+	user.Password = AppConfig.User.DefaultPassword
+	user.Role = "admin"
 
-		var user User
-		user.Username = "novus"
-		user.Password = "Novus123!"
-		user.Role = "admin"
+	//result := db.Where("username = ?", user.Username).FirstOrCreate(&user)
+	//if result.Error != nil {
+	//	fmt.Println(result.Error.Error())
+	//	return
+	//}
 
-		//result := db.Where("username = ?", user.Username).FirstOrCreate(&user)
-		//if result.Error != nil {
-		//	fmt.Println(result.Error.Error())
-		//	return
-		//}
+	addAdminGroup()
+	addUserGroup()
 
-		addAdminGroup()
-		addUserGroup()
-
-		warning, err := addUserToSystem(user)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		fmt.Println(warning)
-
-		// /readUsersFromSystem()
-
-		//user := User{
-		//
-		//	Username: "admin",
-		//	Role:     "admin",
-		//	Email:    "",
-		//	Password: "admin",
-		//}
-		//
-		//db.Create(&user)
-		//
-		//snmpV1V2User := SnmpV1V2cUser{
-		//	Version:   "v2c",
-		//	GroupName: "read_write",
-		//	Community: "myCommunity",
-		//	//	IpVersion:  "ipv4",
-		//	//	Ip4Address: "10.1.10.220",
-		//}
-		//
-		//db.Create(&snmpV1V2User)
-
-		//access := Access{
-		//
-		//	Node: "10.1.10.1",
-		//}
-		//db.Create(&access)
-		//
-		//access = Access{
-		//
-		//	Node: "10.1.10.2",
-		//}
-		//db.Create(&access)
-		//access = Access{
-		//
-		//	Node: "10.1.10.3",
-		//}
-		//
-		//db.Create(&access)
-
+	warning, err := addUserToSystem(user)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
+	fmt.Println(warning)
+
+	// /readUsersFromSystem()
+
+	//user := User{
+	//
+	//	Username: "admin",
+	//	Role:     "admin",
+	//	Email:    "",
+	//	Password: "admin",
+	//}
+	//
+	//db.Create(&user)
+	//
+	//snmpV1V2User := SnmpV1V2cUser{
+	//	Version:   "v2c",
+	//	GroupName: "read_write",
+	//	Community: "myCommunity",
+	//	//	IpVersion:  "ipv4",
+	//	//	Ip4Address: "10.1.10.220",
+	//}
+	//
+	//db.Create(&snmpV1V2User)
+
+	//access := Access{
+	//
+	//	Node: "10.1.10.1",
+	//}
+	//db.Create(&access)
+	//
+	//access = Access{
+	//
+	//	Node: "10.1.10.2",
+	//}
+	//db.Create(&access)
+	//access = Access{
+	//
+	//	Node: "10.1.10.3",
+	//}
+	//
+	//db.Create(&access)
 
 }
