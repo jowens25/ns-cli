42689817cd3fe426de3e9b0e1c55f88d9cfe9946
jowens25 <jowens@novuspower.com>
2025-09-24 17:23:21 -0500

    reworking serial again

--- lib/micro.go
+++ lib/micro.go
@@ -2,74 +2,97 @@ package lib
 
 import "C"
 import (
+	"bufio"
 	"fmt"
-	"log"
-	"strings"
-	"time"
-
-	"go.bug.st/serial"
+	"os"
 )
 
-// command is the actual string so ex $BAUDNV
 func ReadWriteMicro(command string) (string, error) {
 
 	command = command + "\r\n"
 
-	mode := &serial.Mode{
-		BaudRate: AppConfig.Serial.Baudrate,
-		DataBits: 8,
-		Parity:   serial.NoParity,
-		StopBits: serial.OneStopBit,
+	file, err := os.OpenFile(AppConfig.Serial.Port, os.O_RDWR, 0)
+	if err != nil {
+		return "unable to open for write", fmt.Errorf("failed to open for write")
 	}
+	defer file.Close()
 
-	read_data := make([]byte, 1024)
-
-	port, err := serial.Open(AppConfig.Serial.Port, mode)
+	_, err = file.WriteString(command)
 
 	if err != nil {
-		log.Println(AppConfig.Serial.Port, err)
-		return "port open error", err
+		return "unable to write", fmt.Errorf("failed to write")
 	}
-	defer port.Close()
 
-	port.ResetInputBuffer()
-	port.ResetOutputBuffer()
+	scanner := bufio.NewScanner(file)
 
-	_, err = port.Write([]byte(command))
-
-	fmt.Print(command)
-
-	if err != nil {
-		log.Println(err)
-		return "port write error", err
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 
 	}
 
-	err = port.SetReadTimeout(1 * time.Second)
-	if err != nil {
-		log.Println("Failed to set read timeout:", err)
-		return "", err
-	}
-
-	for {
-
-		n, err := port.Read(read_data)
-		if err != nil {
-			log.Println(err)
-		}
-
-		lines := string(read_data[:n])
-
-		if strings.Contains(lines, command[:3]) {
-			responses := strings.Split(lines, "\r\n")
-			for _, response := range responses {
-				if strings.Contains(response, command[:3]) {
-					return strings.TrimSpace(responses[0]), nil
-
-				}
-			}
-
-		}
-	}
-	return "timeout", err
+	return "nothing", nil
 }
+
+// command is the actual string so ex $BAUDNV
+//func ReadWriteMicro(command string) (string, error) {
+//
+//	command = command + "\r\n"
+//
+//	mode := &serial.Mode{
+//		BaudRate: AppConfig.Serial.Baudrate,
+//		DataBits: 8,
+//		Parity:   serial.NoParity,
+//		StopBits: serial.OneStopBit,
+//	}
+//
+//	read_data := make([]byte, 1024)
+//
+//	port, err := serial.Open(AppConfig.Serial.Port, mode)
+//
+//	if err != nil {
+//		log.Println(AppConfig.Serial.Port, err)
+//		return "port open error", err
+//	}
+//	defer port.Close()
+//
+//	port.ResetInputBuffer()
+//	port.ResetOutputBuffer()
+//
+//	_, err = port.Write([]byte(command))
+//
+//	fmt.Print(command)
+//
+//	if err != nil {
+//		log.Println(err)
+//		return "port write error", err
+//
+//	}
+//
+//	err = port.SetReadTimeout(1 * time.Second)
+//	if err != nil {
+//		log.Println("Failed to set read timeout:", err)
+//		return "", err
+//	}
+//
+//	for {
+//
+//		n, err := port.Read(read_data)
+//		if err != nil {
+//			log.Println(err)
+//		}
+//
+//		lines := string(read_data[:n])
+//
+//		if strings.Contains(lines, command[:3]) {
+//			responses := strings.Split(lines, "\r\n")
+//			for _, response := range responses {
+//				if strings.Contains(response, command[:3]) {
+//					return strings.TrimSpace(responses[0]), nil
+//
+//				}
+//			}
+//
+//		}
+//	}
+//	return "timeout", err
+//}
